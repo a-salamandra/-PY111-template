599bd79e4c5a8cd0668d9228aa1c74b7854ba137
a-salamandra <la.februaro@gmail.com>
2022-09-23 17:56:14 +0300

    stairway

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -8,5 +8,10 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :param stairway: list of ints, where each int is a cost of appropriate step
     :return: minimal cost of getting to the top
     """
+    stairway.append(0)
+
+    for i in range(len(stairway)-3,-1,-1):
+        stairway[i] = min(stairway[i] + stairway[i+1], stairway[i] + stairway[i+2])
+
     print(stairway)
-    return 0
+    return min(stairway[0], stairway[1])
